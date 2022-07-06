38441f4764eea524ca4b1ee17a743f9a333230f1
EP <26110043+Prasanna-sippa@users.noreply.github.com>
2022-07-06 07:35:53 +0530

    Create bridges.cpp

---
+++ graphs/bridges.cpp
@@ -0,0 +1,56 @@
+bridge: if removal of an edge breaks the graph into 2 or more components then its calles bridge
+we will use 2 array timeofinsertion and lowest time of insertion
+we can call an edge a bridge if condition satisfies
+low[it]>tin[node] =>bridge
+tc::o(n+e)  sc:o(n)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  #include <bits/stdc++.h>
+using namespace std;
+void dfs(int node, int parent, vector<int> &vis, vector<int> &tin, vector<int> &low, int &timer, vector<int> adj[]) {
+    vis[node] = 1; 
+    tin[node] = low[node] = timer++; 
+    for(auto it: adj[node]) {
+        if(it == parent) continue;
+        
+        if(!vis[it]) {
+            dfs(it, node, vis, tin, low, timer, adj); 
+            low[node] = min(low[node], low[it]); 
+            if(low[it] > tin[node]) {
+                cout << node << " " << it << endl;
+            }
+        } else {
+            low[node] = min(low[node], tin[it]); 
+        }
+    }
+}
+int main() {
+    int n, m;
+    cin >> n >> m; 
+	vector<int> adj[n]; 
+	for(int i = 0;i<m;i++) {
+	    int u, v;
+	    cin >> u >> v; 
+	    adj[u].push_back(v);
+	    adj[v].push_back(u); 
+	}
+	
+	vector<int> tin(n, -1);
+	vector<int> low(n, -1); 
+	vector<int> vis(n, 0); 
+	int timer = 0; 
+	for(int i = 0;i<n;i++) {
+	    if(!vis[i]) {
+	        dfs(i, -1, vis, tin, low, timer, adj); 
+	    }
+	}
+	
+	return 0;
+}
