02ab6c869be2227358a41237ee81f7cc031631ec
EP <26110043+Prasanna-sippa@users.noreply.github.com>
2022-07-22 11:48:45 +0530

    Add files via upload

---
+++ two_pointers/valid_palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0,z=s.size();
+        if(s.empty()){
+            return true;
+        }
+        while(i<z){
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[z])){
+                z--;
+                continue;
+            }
+            if(tolower(s[i++])!=tolower(s[z--])){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+Explanation: Isalnum does exactly what is sounds like, checks if the ASCII value passed in has a character equivalent to a number of letter. It returns non-zero for true, and zero for false.
