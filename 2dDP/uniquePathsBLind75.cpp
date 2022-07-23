fa02a3aa33b1017dbdcfb3ae7d522ef974fd2d2f
EP <26110043+Prasanna-sippa@users.noreply.github.com>
2022-07-23 23:38:12 +0530

    Update uniquePathsBLind75.cpp

--- 2dDP/uniquePathsBLind75.cpp
+++ 2dDP/uniquePathsBLind75.cpp
@@ -22,20 +22,36 @@ public:
 
 ///another
 
-#include<bits/stdc++.h>
-using namespace std;
-class Solution {
+/*
+    Given grid, return # of unique paths from top-left to bottom-right
+    Ex. m = 3, n = 2 -> 3 unique paths (R->D->D, D->D->R, D->R->D)
 
+    DP: edges have 1 unique path, inner cells consider where it comes from
+    Recurrence relation: grid[i][j] = grid[i-1][j] + grid[i][j-1]
+
+    Time: O(m x n)
+    Space: O(m x n)
+*/
+
+class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,1));
-        for(int i=0;i<m;i++)dp[i][0]=1;
-        for(int i=0;i<n;i++)dp[0][i]=1;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        vector<vector<int>> grid(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < m; i++) {
+            grid[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            grid[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
             }
         }
-        return dp[m-1][n-1];
+        
+        return grid[m - 1][n - 1];
     }
 };
+
+
