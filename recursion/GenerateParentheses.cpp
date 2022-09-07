2c4e7867986d22567f9228bc04ca8a925d71a276
EP <26110043+Prasanna-sippa@users.noreply.github.com>
2022-09-07 21:48:42 +0530

    Create GenerateParentheses.cpp

---
+++ recursion/GenerateParentheses.cpp
@@ -0,0 +1,48 @@
+
+
+
+Given an integer N representing the number of pairs of parentheses, the task is to generate all combinations of well-formed(balanced) parentheses.
+
+
+Example 1:
+
+Input:
+N = 3
+Output:
+((()))
+(()())
+(())()
+()(())
+()()()
+
+
+class Solution
+{
+    public:
+    void helper(string cur,int o,int c,int n,vector<string>& res){
+        if(c==o && c==n){
+            res.push_back(cur);
+            return;
+        }
+        //open
+        if(o<n){
+            helper(cur+"(",o+1,c,n,res);
+            
+        }
+        //close
+         if(o>c){
+            helper(cur+")",o,c+1,n,res);
+            
+        }
+        
+    }
+    vector<string> AllParenthesis(int n) 
+    {
+        // Your code goes here 
+        vector<string> res;
+        
+        helper("",0,0,n,res);
+        return res;
+        
+    }
+};
