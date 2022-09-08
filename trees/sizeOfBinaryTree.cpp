bd4b43b9cd8c0ca4fbceed08b89536176d762490
EP <26110043+Prasanna-sippa@users.noreply.github.com>
2022-09-08 22:39:30 +0530

    Update sizeOfBinaryTree.cpp

--- trees/sizeOfBinaryTree.cpp
+++ trees/sizeOfBinaryTree.cpp
@@ -6,3 +6,16 @@ int getSize(Node* node)
             +(node->right ? getSize(node->right) : 0);
    
 }
+
+
+//height of tree
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        // code here 
+        
+        return 1+max((node->left ? height(node->left) : 0),
+                 (node->right ? height(node->right):0));
+    }
+};
