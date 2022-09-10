2e16387da566c82600f24e7b45a086cc0f6fd279
EP <26110043+Prasanna-sippa@users.noreply.github.com>
2022-09-10 16:28:53 +0530

    Update searchMatrix2D.cpp

--- binary search/searchMatrix2D.cpp
+++ binary search/searchMatrix2D.cpp
@@ -18,3 +18,26 @@ public:
         return false;
     }
 };
+
+
+
+//////////////////
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int X) {
+        int N=mat.size(),M=mat[0].size();
+        int l=0,h=M-1;
+	    while(l<N && h>=0){
+	        if(mat[l][h]==X){
+	            return true;
+	        }
+	        else if(mat[l][h]<X){
+	            l++;
+	        }
+	        else{
+	            h--;
+	        }
+	    }
+	    return 0;
+    }
+};
